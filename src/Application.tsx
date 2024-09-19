4de41331394c1a1898cf792f1a6d4d6ba6b70199
Lyumih <m.ivanov1225@gmail.com>
2024-09-19 21:06:14 +0300

    1

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,31 +1,7 @@
 import { App, Button, Card, Flex, InputNumber, Layout, List, Space, Switch, Typography } from 'antd'
 import { create } from 'zustand'
+import { useShallow } from 'zustand/react/shallow'
 
-interface GameState {
-  battlesCount: number,
-  spendTime: number,
-  increaseBattlesCount: (by: number) => void,
-  reset: () => void,
-  items: {
-    type: 'skill' | 'weapon' | 'modifier'
-    name: string
-    description: string,
-    level: number
-    lastChance: number
-    strategy: 'use' | 'win' | 'useAny'
-    uses: number
-  }[],
-  updateItems: (newItems: GameState['items']) => void
-  settings: {
-    effectiveMaxLevel: number
-    minChance: number
-    modifierStepLevel: number
-    time: number
-  },
-  updateSettings: (newSettings: GameState['settings']) => void
-  history: string[],
-  addHistory: (text: string) => void
-}
 
 const itemsDefault: GameState['items'] = [
   {
@@ -87,6 +63,32 @@ const modifiersDefaults: GameState['items'] = [
   },
 ]
 
+interface GameState {
+  battlesCount: number,
+  spendTime: number,
+  increaseBattlesCount: (by: number) => void,
+  reset: () => void,
+  items: {
+    type: 'skill' | 'weapon' | 'modifier'
+    name: string
+    description: string,
+    level: number
+    lastChance: number
+    strategy: 'use' | 'win' | 'useAny'
+    uses: number
+  }[],
+  updateItems: (newItems: GameState['items']) => void
+  settings: {
+    effectiveMaxLevel: number
+    minChance: number
+    modifierStepLevel: number
+    time: number
+  },
+  updateSettings: (newSettings: GameState['settings']) => void
+  history: string[],
+  addHistory: (text: string) => void
+}
+
 const useGameStore = create<GameState>()((set) => ({
   battlesCount: 0,
   spendTime: 0,
@@ -109,7 +111,7 @@ let intervalBattle = 0
 
 function Application() {
 
-  const store = useGameStore();
+  const store = useGameStore(useShallow(state => state));
 
   const handleChangeSettings = (prop: string) => (value: number | null) => {
     store.updateSettings({ ...store.settings, [prop]: value || 0 })
@@ -125,8 +127,7 @@ function Application() {
   }
 
   const handleLevelUp = (indexItem: number, levels: number, lastChance: number,) => {
-    console.log('level up', indexItem, levels, store.items)
-    store.addHistory(`Уровень ${store.items[indexItem].name} ${store.items[indexItem].level}  повышен на ${levels} с шансом (${lastChance})`)
+    store.addHistory(`Уровень ${store.items[indexItem].name} ${store.items[indexItem].level}  повышен на ${levels} с шансом (${lastChance}) в битве ${store.battlesCount}`)
     store.updateItems(store.items.map((item, index) => index === indexItem ? { ...item, level: item.level + levels, lastChance: lastChance } : item))
   }
 
@@ -142,13 +143,13 @@ function Application() {
 
   const makeWin = (count = 1) => {
     for (let i = 0; i < count; i++) {
+      store.increaseBattlesCount(1)
       chanceLevelUp(0, 1)
       // handleLevelUp(1, 1, -1)
       // chanceLevelUp(1, 1)
       // chanceLevelUp(2, 1)
       // chanceLevelUp(1, 1)
       // chanceLevelUp(0, 1)
-      store.increaseBattlesCount(1)
     }
   }
 
@@ -165,86 +166,86 @@ function Application() {
   return (
     <App>
       <Layout>
-        <Layout.Header style={{ background: 'transparent' }}>
-          <Typography.Title level={2}>Калькулятор игровой системы</Typography.Title>
-        </Layout.Header>
-        <Layout.Content>
-          <Flex gap='large' vertical>
-
-            {/* Панели */}
-            <Flex gap='small' align='space-between'>
-              <Card title="Панель">
-                <Flex vertical gap='small'>
-                  <Space>
-                    <Button onClick={() => makeWin(1)}>Победа</Button>
-                    <Button onClick={() => makeWin(10)}>+10 побед</Button>
-                    <Button onClick={() => makeWin(100)}>+100 побед</Button>
-                    <Button onClick={() => makeWin(1000)}>+1000 побед</Button>
-                  </Space>
-                  <Space>
-                    <Switch checkedChildren="Старт" unCheckedChildren="Стоп" onChange={handleStart} />
-                    <Button onClick={store.reset}>Сбросить</Button>
-                  </Space>
-                </Flex>
-              </Card>
-              <Card title="Информация">
-                <Flex gap='small' vertical>
-                  <Typography.Text>Боёв: {store.battlesCount}</Typography.Text>
-                  <Typography.Text>Время: {getTimeFormat()}</Typography.Text>
-                </Flex>
-              </Card>
-              <Card title="Настройки">
-                <Flex gap='small' vertical>
-                  <Space>
-                    <Typography.Text>Предел</Typography.Text> <InputNumber value={store.settings.effectiveMaxLevel} onChange={handleChangeSettings('effectiveMaxLevel')} addonAfter='ур.' />
-                  </Space>
-                  <Space>
-                    <Typography.Text>Мин. шанс</Typography.Text> <InputNumber value={store.settings.minChance} onChange={handleChangeSettings('minChance')} addonAfter='%' />
-                  </Space>
-                  <Space>
-                    <Typography.Text>Шаг модификаторов</Typography.Text> <InputNumber value={store.settings.modifierStepLevel} onChange={handleChangeSettings('modifierStepLevel')} addonAfter='ур.' />
-                  </Space>
-                  <Space>
-                    <Typography.Text>Время</Typography.Text> <InputNumber value={store.settings.time} onChange={handleChangeSettings('time')} addonAfter='сек.' />
-                  </Space>
-                </Flex>
-              </Card>
-            </Flex>
-
+        <Layout.Header style={{ background: 'transparent' }}>Калькулятор игровой системы</Layout.Header>
+        <Layout>
+          <Layout.Sider width='25%'>
             {/* ИСТОРИЯ */}
-            <Card title="История">
+            <Card style={{ height: '100%' }} title="История">
               <List
-                style={{ maxHeight: 300, overflow: 'auto' }}
+                style={{ maxHeight: '800px', overflow: 'auto' }}
                 bordered
                 dataSource={store.history}
                 renderItem={(item) => <List.Item>{item}</List.Item>} />
             </Card>
+          </Layout.Sider>
+          <Layout.Content>
+            <Flex gap='large' vertical>
 
-            {/* УМЕНИЯ */}
-            <Flex gap='small'>
-              {store.items.map((item, index) =>
-                <Card key={item.name} title={item.type === 'skill' ? 'Умение' : 'Оружие'} extra={<Button onClick={() => handleLevelUp(index, 25, -1)}>+ 25 уровней</Button>}>
-                  <Flex vertical>
-                    <Typography.Text>Ур. {item.level}. Шанс {100 - item.level}%. Предыдущий шанс: {item.lastChance}%</Typography.Text>
-                    <Typography.Text>Описание: {item.description}</Typography.Text>
-                    <Typography.Text>Использований: {item.uses} </Typography.Text>
-                    <Typography.Text>Улучшение: при использовании {item.strategy === 'useAny' && 'и атаке'} </Typography.Text>
-                    {modifiersDefaults.map(modifier =>
-                      <Card key={index + modifier.name} title={'Модификатор: ' + modifier.name}>
-                        <Flex vertical>
-                          <Typography.Text>Ур. {modifier.level}. Шанс {modifier.level}%</Typography.Text>
-                          <Typography.Text>Описание: {modifier.description}</Typography.Text>
-                          <Typography.Text>Использований: {modifier.uses} </Typography.Text>
-                          <Typography.Text>Улучшение: при победе</Typography.Text>
-                        </Flex>
-                      </Card>
-                    )}
+              {/* Панели */}
+              <Flex gap='small' align='space-between'>
+                <Card title="Панель">
+                  <Flex vertical gap='small'>
+                    <Space>
+                      <Button onClick={() => makeWin(1)}>Победа</Button>
+                      <Button onClick={() => makeWin(10)}>+10 побед</Button>
+                      <Button onClick={() => makeWin(100)}>+100 побед</Button>
+                      <Button onClick={() => makeWin(1000)}>+1000 побед</Button>
+                    </Space>
+                    <Space>
+                      <Switch checkedChildren="Старт" unCheckedChildren="Стоп" onChange={handleStart} />
+                      <Button onClick={store.reset}>Сбросить</Button>
+                    </Space>
+
+                    <Flex gap='small' vertical>
+                      <Typography.Text>Боёв: {store.battlesCount}</Typography.Text>
+                      <Typography.Text>Время: {getTimeFormat()}</Typography.Text>
+                    </Flex>
                   </Flex>
                 </Card>
-              )}
+                <Card title="Настройки">
+                  <Flex gap='small' vertical>
+                    <Space>
+                      <Typography.Text>Предел</Typography.Text> <InputNumber value={store.settings.effectiveMaxLevel} onChange={handleChangeSettings('effectiveMaxLevel')} addonAfter='ур.' />
+                    </Space>
+                    <Space>
+                      <Typography.Text>Мин. шанс</Typography.Text> <InputNumber value={store.settings.minChance} onChange={handleChangeSettings('minChance')} addonAfter='%' />
+                    </Space>
+                    <Space>
+                      <Typography.Text>Модификатор</Typography.Text> <InputNumber value={store.settings.modifierStepLevel} onChange={handleChangeSettings('modifierStepLevel')} addonAfter='ур.' />
+                    </Space>
+                    <Space>
+                      <Typography.Text>Время</Typography.Text> <InputNumber value={store.settings.time} onChange={handleChangeSettings('time')} addonAfter='сек.' />
+                    </Space>
+                  </Flex>
+                </Card>
+              </Flex>
+
+              {/* УМЕНИЯ */}
+              <Flex gap='small'>
+                {store.items.map((item, index) =>
+                  <Card key={item.name} title={item.type === 'skill' ? 'Умение' : 'Оружие'} extra={<Button onClick={() => handleLevelUp(index, 25, -1)}>+ 25 уровней</Button>}>
+                    <Flex vertical>
+                      <Typography.Text>Ур. {item.level}. Шанс {100 - item.level}%. Предыдущий шанс: {item.lastChance}%</Typography.Text>
+                      <Typography.Text>Описание: {item.description}</Typography.Text>
+                      <Typography.Text>Использований: {item.uses} </Typography.Text>
+                      <Typography.Text>Улучшение: при использовании {item.strategy === 'useAny' && 'и атаке'} </Typography.Text>
+                      {modifiersDefaults.map(modifier =>
+                        <Card key={index + modifier.name} title={'Модификатор: ' + modifier.name}>
+                          <Flex vertical>
+                            <Typography.Text>Ур. {modifier.level}. Шанс {modifier.level}%</Typography.Text>
+                            <Typography.Text>Описание: {modifier.description}</Typography.Text>
+                            <Typography.Text>Использований: {modifier.uses} </Typography.Text>
+                            <Typography.Text>Улучшение: при победе</Typography.Text>
+                          </Flex>
+                        </Card>
+                      )}
+                    </Flex>
+                  </Card>
+                )}
+              </Flex>
             </Flex>
-          </Flex>
-        </Layout.Content>
+          </Layout.Content>
+        </Layout>
         <Layout.Footer>
           <Typography.Link href='https://t.me/mikhail_eco_coach' target='_blank'>Телеграмм</Typography.Link>
         </Layout.Footer>
